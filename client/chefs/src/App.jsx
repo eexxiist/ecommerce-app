6d63d5fbc87b6c17e8ce5876aba83ac77210e36c
eexxiist <mego_dilya@mail.ru>
2026-04-24 20:45:28 +0300

    написаны роуты, создан стор для mobx

--- client/chefs/src/App.jsx
+++ client/chefs/src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      WORK
-    </>
-  )
+  return <AppRouter />;
 }
 
-export default App
+export default App;
--- client/chefs/src/components/AppRouter.jsx
+++ client/chefs/src/components/AppRouter.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { authRouter, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
 
 const AppRouter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const isAuth = false;
+    return (
+        <Routes>
+            {isAuth &&
+                authRouter.map(({ path, Component }) => (
+                    <Route key={path} path={path} element={<Component />} />
+                ))}
+            {publicRoutes.map(({ path, Component }) => (
+                <Route key={path} path={path} element={<Component />} />
+            ))}
+            <Route path="*" element={<Navigate to={SHOP_ROUTE} />} />
+        </Routes>
+    );
+};
 
-export default AppRouter
+export default AppRouter;
