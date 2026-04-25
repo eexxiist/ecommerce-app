56e97a2822bde033e858cea5dac3c320c118769e
eexxiist <mego_dilya@mail.ru>
2026-04-25 16:15:55 +0300

    написан компонент брендов

--- client/chefs/src/components/BrandBar.jsx
+++ client/chefs/src/components/BrandBar.jsx
@@ -1,8 +1,33 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useContext } from "react";
+import { Card, Row } from "react-bootstrap";
+import { Context } from "../main";
 
 const BrandBar = observer(() => {
-    return <div></div>;
+    const { device } = useContext(Context);
+    return (
+        <Row className="d-flex">
+            {device.brands.map((brand) => (
+                <Card
+                    style={{
+                        cursor: "pointer",
+                        width: "120px",
+                        textAlign: "center",
+                    }}
+                    border={
+                        brand.id === device.selectedBrand.id
+                            ? "danger"
+                            : "light"
+                    }
+                    key={brand.id}
+                    className="p-3"
+                    onClick={() => device.setSelectedBrand(brand)}
+                >
+                    {brand.name}
+                </Card>
+            ))}
+        </Row>
+    );
 });
 
 export default BrandBar;
--- client/chefs/src/store/deviceStore.js
+++ client/chefs/src/store/deviceStore.js
@@ -11,6 +11,8 @@ export default class DeviceStore {
         this._brands = [
             { id: 1, name: "Samsung" },
             { id: 2, name: "Apple" },
+            { id: 3, name: "Lenovo" },
+            { id: 4, name: "Asus" },
         ];
         this._devices = [
             {
@@ -43,6 +45,7 @@ export default class DeviceStore {
             },
         ];
         this._selectedType = {};
+        this._selectedBrand = {};
         makeAutoObservable(this);
     }
 
@@ -60,6 +63,9 @@ export default class DeviceStore {
     setSelectedType(type) {
         this._selectedType = type;
     }
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand;
+    }
 
     get types() {
         return this._types;
@@ -72,5 +78,8 @@ export default class DeviceStore {
     }
     get selectedType() {
         return this._selectedType;
+    }
+    get selectedBrand() {
+        return this._selectedBrand;
     }
 }
