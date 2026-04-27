ff68c19d607809ef6d4ab9237ac2f8943773b5ba
eexxiist <mego_dilya@mail.ru>
2026-04-27 11:22:42 +0300

    написана страница админа  и модальные окна

--- client/chefs/src/components/NavBar.jsx
+++ client/chefs/src/components/NavBar.jsx
@@ -2,11 +2,13 @@ import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import React, { useContext } from "react";
 import { Context } from "../main";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = observer(() => {
     const { user } = useContext(Context);
+    const history = useNavigate();
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
@@ -16,16 +18,28 @@ const NavBar = observer(() => {
 
                 {user.isAuth ? (
                     <Nav className="ml-auto" style={{ color: "white" }}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className="ml-4">
-                            Войти
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history(LOGIN_ROUTE)}
+                            className="ml-4"
+                        >
+                            Выйти
                         </Button>
                     </Nav>
                 ) : (
                     <Nav className="ml-auto" style={{ color: "white" }}>
                         <Button
                             variant={"outline-light"}
-                            onClick={() => user.setIsAuth(true)}
+                            onClick={() => {
+                                user.setIsAuth(true);
+                                history(LOGIN_ROUTE), console.log(user.isAuth);
+                            }}
                         >
                             Авторизация
                         </Button>
